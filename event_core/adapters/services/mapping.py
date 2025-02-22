5fb862df39148d62d8b0cf02c8cf0055a2ff3ad5
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-22 19:16:14 +0800

    add delete method to mapping adapter

--- event_core/adapters/services/mapping.py
+++ event_core/adapters/services/mapping.py
@@ -9,16 +9,20 @@ from event_core.config import get_redis_mapping_connection_params
 
 logger = logging.getLogger(__name__)
 
-Value: TypeAlias = Union[str, int, float]
+ValueT: TypeAlias = Union[str, int, float]
 
 
 class AbstractMapper(ABC):
     @abstractmethod
-    def set(self, key: str, val: Value) -> None:
+    def set(self, key: str, val: ValueT) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    def get(self, key: str) -> Value:
+    def get(self, key: str) -> ValueT:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def delete(self, key: str) -> None:
         raise NotImplementedError
 
 
@@ -26,10 +30,13 @@ class RedisMapper(AbstractMapper):
     def __init__(self):
         self._r = redis.Redis(**get_redis_mapping_connection_params())
 
-    def set(self, key: str, val: Value) -> None:
-        self._r.set(name=key, value=val)
+    def set(self, key: str, val: ValueT) -> None:
+        self._r.set(name=key, valueT=val)
 
-    def get(self, key: str) -> Value:
+    def get(self, key: str) -> ValueT:
         if (v := self._r.get(name=key)) is None:
             raise KeyNotExists(f"Key {key} does not exist")
         return v
+
+    def delete(self, key: str) -> None:
+        self._r.delete(key)
