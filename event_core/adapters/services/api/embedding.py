76b13a92c1ac808561ba9e81851663b841d8fd6a
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-25 15:01:49 +0800

    add n_rank and n_cands for query

--- event_core/adapters/services/api/embedding.py
+++ event_core/adapters/services/api/embedding.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Dict, List
+from typing import List
 from urllib.parse import urljoin
 
 import requests
@@ -11,9 +11,16 @@ logger = logging.getLogger(__name__)
 API_URL = get_embedding_service_api_url()
 
 
-def query_text(user: str, text: str) -> Dict[str, List[str]]:
+def query_text(user: str, text: str, n_cands: int, n_rank: int) -> List[str]:
     logger.info(f'Querying text="{text}" in {user} namespace')
     res = requests.get(
-        urljoin(API_URL, "query/text"), params={"user": user, "text": text}
+        urljoin(API_URL, "query/text"),
+        params={
+            "user": user,
+            "text": text,
+            "n_cands": n_cands,
+            "n_rank": n_rank,
+        },
     )
-    return res.json()
+    keys: List[str] = res.json()
+    return keys
