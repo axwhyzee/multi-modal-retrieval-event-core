762bec0fe22b9795e2535c384fe34b5140ce6120
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-28 11:41:24 +0800

    add FileExt domain type

--- event_core/domain/types.py
+++ event_core/domain/types.py
@@ -1,4 +1,5 @@
 from enum import StrEnum
+from typing import Dict
 
 
 class ObjectType(StrEnum):
@@ -12,3 +13,20 @@ class Modal(StrEnum):
     TEXT = "TEXT"
     IMAGE = "IMAGE"
     VIDEO = "VIDEO"
+
+
+class FileExt(StrEnum):
+    JPG = ".jpg"
+    JPEG = ".jpeg"
+    PNG = ".png"
+    MP4 = ".mp4"
+    TXT = ".txt"
+
+
+MODAL_FACTORY: Dict[FileExt, Modal] = {
+    FileExt.JPG: Modal.IMAGE,
+    FileExt.JPEG: Modal.IMAGE,
+    FileExt.PNG: Modal.IMAGE,
+    FileExt.MP4: Modal.VIDEO,
+    FileExt.TXT: Modal.TEXT,
+}
