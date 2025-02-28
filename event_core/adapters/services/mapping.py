3e8b567e268010cc74c48a0852d7a6de2c3efd7c
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-28 12:31:30 +0800

    add namespace to mappings

--- event_core/adapters/services/mapping.py
+++ event_core/adapters/services/mapping.py
@@ -6,23 +6,28 @@ import redis
 
 from event_core.adapters.services.exceptions import KeyNotExists
 from event_core.config import get_redis_mapping_connection_params
+from event_core.domain.types import ObjectType
 
 logger = logging.getLogger(__name__)
 
 ValueT: TypeAlias = Union[str, int, float]
 
 
+def _namespace_from_obj_types(from_type: ObjectType, to_type: ObjectType) -> str:
+    return f'{from_type}__{to_type}'
+
+
 class AbstractMapper(ABC):
     @abstractmethod
-    def set(self, key: str, val: ValueT) -> None:
+    def set(self, from_type: ObjectType, to_type: ObjectType, str, val: ValueT) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    def get(self, key: str) -> ValueT:
+    def get(self, from_type: ObjectType, to_type: ObjectType, key: str) -> ValueT:
         raise NotImplementedError
 
     @abstractmethod
-    def delete(self, key: str) -> None:
+    def delete(self, from_type: ObjectType, to_type: ObjectType, key: str) -> None:
         raise NotImplementedError
 
 
@@ -30,13 +35,16 @@ class RedisMapper(AbstractMapper):
     def __init__(self):
         self._r = redis.Redis(**get_redis_mapping_connection_params())
 
-    def set(self, key: str, val: ValueT) -> None:
-        self._r.set(name=key, value=val)
+    def set(self, from_type: ObjectType, to_type: ObjectType, key: str, val: ValueT) -> None:
+        namespace = _namespace_from_obj_types(from_type, to_type)
+        self._r.hset(name=namespace, key=key, value=val)
 
-    def get(self, key: str) -> ValueT:
-        if (v := self._r.get(name=key)) is None:
+    def get(self, from_type: ObjectType, to_type: ObjectType, key: str) -> ValueT:
+        namespace = _namespace_from_obj_types(from_type, to_type)
+        if (v := self._r.hget(name=namespace, key=key)) is None:
             raise KeyNotExists(f"Key {key} does not exist")
         return v
 
-    def delete(self, key: str) -> None:
-        self._r.delete(key)
+    def delete(self, from_type: ObjectType, to_type: ObjectType, key: str) -> None:
+        namespace = _namespace_from_obj_types(from_type, to_type)
+        self._r.hdel(name=namespace, key=key)
